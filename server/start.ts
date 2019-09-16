53a307e124e864dc1743ff3547097fd6a9513817
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2019-09-16 14:48:42 +0200

    Implemented Cloud Run Docker image

--- server/start.ts
+++ server/start.ts
@@ -1,8 +1,6 @@
 import BucksServer from './BucksServer';
-import dotenv from 'dotenv';
-dotenv.config();
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 const clientPath = (process.env.NODE_ENV === 'production') ? './public' : '../client/public';
 const exampleServer = new BucksServer(clientPath);
 exampleServer.start(port);
